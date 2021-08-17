a24e10d6711164a3aa073d3cb46118f6acc1a430
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-08-17 22:08:49 +1000

    Comment an unused variable in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 // TODO: write code here
 
 // comment this to pass build
-const unusedVariable = 'variable';
+// const unusedVariable = 'variable';
 
 // for demonstration purpose only
 export default function demo(value) {
